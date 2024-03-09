c6661bbb5ce7b775a1f46448ec893efdfa8c51b9
AzrinHakimi0 <azrinhakimi08@outlook.com>
2024-03-09 19:08:27 +0800

    page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-wrap bg-slate-100  m-3 rounded-3xl">
+    <div className="flex flex-1 flex-wrap bg-slate-100  m-3 rounded-3xl md:justify-normal md:items-start justify-center items-center ">
         <AddExpense/>
         <DoughnutChart/>
     </div>
